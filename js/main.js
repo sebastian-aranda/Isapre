124574dd553fe77152859f7b0f0920f4e7b23f30
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2016-04-19 20:01:22 -0300

    Testing

--- js/main.js
+++ js/main.js
@@ -27,13 +27,13 @@ $(function(){
 });*/
 
 $(function(){
-	var sliderItemWidth = $('.content-slide-item').outerWidth();
-
 	$('a').on("click", function(e){
 		e.preventDefault();
+
+		var sliderItemWidth = $('.content-slide-item').outerWidth();
 		var target = $(this).attr('href').slice(1);
 
-		var active_pos = $('.active').index();
+		var active_pos = $('.content-slide-item.active').index();
 		var new_pos;
 		$('.content-slide-item').each(function(){
 			if ($(this).attr('id') == target){
@@ -50,7 +50,48 @@ $(function(){
 		else if (new_pos > active_pos)
 			$('.content-slide').animate({left: '-='+sliderItemWidth*Math.abs(active_pos-new_pos)}, 500);
 	});
+
+	var fadeSpeed = 2000;
+
+	$('#contact-prev').on("click", function(){
+		var actual_step = $('.contact-step.active').index();
+		var new_step = actual_step-1;
+
+		var nElements = $('.contact-form .contact-step').length;		
+		if (new_step<0) return;
+
+		$('.contact-step.active').fadeOut(fadeSpeed);
+		$('.contact-step').each(function(){
+			if ($(this).index() == new_step){
+				$(this).addClass('active');
+				$(this).fadeIn(fadeSpeed);
+			}
+			else{
+				$(this).removeClass('active');
+			}
+		});
+	});
+
+	$('#contact-next').on("click", function(){
+		var actual_step = $('.contact-step.active').index();
+		var new_step = actual_step+1;
+
+		var nElements = $('.contact-form .contact-step').length;
+		if (new_step>(nElements-1)) return;
+
+		$('.contact-step.active').fadeOut(fadeSpeed);
+		$('.contact-step').each(function(){
+			if ($(this).index() == new_step){
+				$(this).addClass('active');
+				$(this).fadeIn(fadeSpeed);
+			}
+			else{
+				$(this).removeClass('active');
+			}
+		});
+	});
 });
+
 
 /*
 $(window).on('hashchange',function(){
